a55fa276859f39ce728c52248203e35ac68361b5
Виктор <lucckyboy713@gmail.com>
2019-12-03 21:52:57 +0300

    Добавьте файлы проекта.

---
+++ Stepik/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepik
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b ==  c + d || a ==b && c == d  ? "YES" : "NO") ;
+        }
+    }
+}
+        
