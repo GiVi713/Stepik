58fae44e136789c5faee961215f0ab39ad48bcf1
Виктор <lucckyboy713@gmail.com>
2019-12-19 20:22:03 +0300

    lesson423

---
+++ Lesson423/Lesson423/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lesson423
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("2");
+            }
+            else if ( x < 0 && y < 0)
+            {
+                Console.WriteLine("3");
+            }
+            else
+            {
+                Console.WriteLine("4");
+            }
+        }
+    }
+}
