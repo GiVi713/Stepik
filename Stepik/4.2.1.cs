3990688116c9060578d820a812557e0980952f33
Виктор <lucckyboy713@gmail.com>
2019-12-19 19:24:16 +0300

    2 задачи

---
+++ Stepik/4.2.1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepik
+{
+    class Class2
+    {
+        private static void Class()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (-3 < x && x < 1)
+            {
+                Console.WriteLine("Yes");
+            }
+                if ( 5 < x && x < 9)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }  
+        }
+    }
+
---
+++ lesson421/lesson421/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lesson421
+{
+    class Program
+    {
+        static void Main()
+        {
+            {
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (-3 <= x && x <= 1 | 5 <= x && x <= 9)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
