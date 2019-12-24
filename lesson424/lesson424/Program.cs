d0d6db7494089317d7564074adbff01ab3031dc1
Виктор <lucckyboy713@gmail.com>
2019-12-24 16:13:58 +0300

    done 4.2

---
+++ lesson424/lesson424/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lesson424
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = b * b - 4 * a * c;
+            double x1, x2;
+            if ( d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                if (x1 > x2)
+                {
+                    Console.WriteLine(x2);
+                    Console.WriteLine(x1);
+                }
+                else
+                {
+                    Console.WriteLine(x1);
+                    Console.WriteLine(x2);
+                }
+            }
+            if (d == 0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine(x1);
+            }
+        }
+    }
+}
---
+++ lesson425/lesson425/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lesson425
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Convert.ToString(Console.ReadLine());
+            switch (month)
+            {
+                case "1": Console.WriteLine(31);break;
+                case "2": Console.WriteLine(29); break;
+                case "3": Console.WriteLine(31); break;
+                case "4": Console.WriteLine(30); break;
+                case "5": Console.WriteLine(31); break;
+                case "6": Console.WriteLine(30); break;
+                case "7": Console.WriteLine(31); break;
+                case "8": Console.WriteLine(31); break;
+                case "9": Console.WriteLine(30); break;
+                case "10": Console.WriteLine(31); break;
+                case "11": Console.WriteLine(30); break;
+                case "12": Console.WriteLine(31); break;
+            }
+        }
+    }
+}
