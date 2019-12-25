d721e61955b420fc8fff2d8a4f6db6c01879422c
Виктор <lucckyboy713@gmail.com>
2019-12-25 15:17:47 +0300

    task 1 done

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if ( num % 2 == 0)
+            {
+                Console.WriteLine(num + 2);
+            }
+            else
+            {
+                Console.WriteLine(num + 1);
+            }
         }
     }
 }
